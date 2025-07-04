aa046877e36ad44e0448e297b6f655bacb434028
Erick-Quiroz <quiroz.erick@outlook.com>
2025-07-03 20:19:56 -0400

    Agregar encabezado y comentario al script run.sh para mejorar la claridad y la documentación del proceso de ejecución del servidor Minecraft.

--- run.sh
+++ run.sh
@@ -1,1 +1,4 @@
-java @user_jvm_args.txt @libraries/net/minecraftforge/forge/1.20.1-47.3.22/unix_args.txt "$@" >> /dev/stdout 2>&1
+#!/bin/bash
+
+# Ejecuta el servidor Minecraft con Forge y redirige logs al stdout para que Docker los capture
+exec java @user_jvm_args.txt @libraries/net/minecraftforge/forge/1.20.1-47.3.22/unix_args.txt "$@" >> /dev/stdout 2>&1
