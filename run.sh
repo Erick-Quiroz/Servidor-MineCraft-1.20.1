b03a97f4d9454f441cf9c2c622ff005482e27767
Erick-Quiroz <quiroz.erick@outlook.com>
2025-07-06 14:14:00 -0400

    Refactor code structure for improved readability and maintainability; removed redundant code blocks and optimized existing functions.

--- run.sh
+++ run.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 # Inicia el servidor Minecraft en segundo plano
-java -jar INICIADOR.jar nogui &
+java -jar INICIADOR.jar nogui
+# &
 
 # Espera un poco para que se cree el archivo de logs
-sleep 2
+# sleep 2
 
 # Redirige los logs del archivo al stdout para Docker
-tail -n 100 -f logs/latest.log
+# tail -n 100 -f logs/latest.log
